5599ff1c13a4b9d1bc26adf27d0502ff194c62f1
kainyzrwaw <sydney.wawrzyniak@kellogg.northwestern.edu>
2022-05-18 01:46:37 +0000

    made changes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+
+    def index
+        @posts = Post.all
+      end
+    
+      def new
+        @post = Post.new
+      end
+    
+      def create
+        @post = Post.new
+        @post["title"] = params["post"]["title"]
+        @post["description"] = params["post"]["description"]
+        @post["date"] = params["post"]["posted_on"]
+        @post.save
+        redirect_to "/posts"
+      end
+    
+end
